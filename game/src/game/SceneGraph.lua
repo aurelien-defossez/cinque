37c991d3a2698ae53bb4ec8d78bce874f3c061dc
Aurelien Defossez <defossez.aurelien@gmail.com>
2013-11-23 22:55:08 +0100

    Score increases

--- game/src/game/SceneGraph.lua
+++ game/src/game/SceneGraph.lua
@@ -20,14 +20,16 @@ local audio = require("audio")
 -- Class attributes
 -----------------------------------------------------------------------------------------
 
-local scorePosition = vec2(300, 20)
+local euroPosition = vec2(280, 20)
+local centsPosition = vec2(300, 20)
 local scoreSize = 20
 local scoreColor = { 240, 255, 200 }
 
 local levels = { 2, 3, 4, 5, 6, 7, 8, 9 }
+local tipLimits = { 0.05, 0.15, 0.30 }
 local tips = {
 	{ 0.00, 0.00, 0.00 },	-- 1
-	{ 0.10, 0.10, 0.10 },	-- 2
+	{ 0.05, 0.10, 0.20 },	-- 2
 	{ 0.20, 0.50, 1.00 },	-- 3
 	{ 0.10, 0.10, 0.20 },	-- 4
 	{ 0.50, 1.00, 2.00 },	-- 5
@@ -37,6 +39,8 @@ local tips = {
 	{ 1.00, 2.00, 5.00 },	-- 9
 	{ 0.50, 1.00, 2.00 },	-- 10
 }
+
+local abs = math.abs
 
 -----------------------------------------------------------------------------------------
 -- Initialization and Destruction
@@ -49,6 +53,7 @@ function Class.create(options)
 	self.gestureDetector = GestureDetector.create()
 	self.rogueElements = {}
 	self.currentLevel = 1
+	self.score = 0
 
 	-- Create background
 	self.background = Rectangle.create{
@@ -64,9 +69,16 @@ function Class.create(options)
 	self.crowd = Crowd.create{}
 
 	-- Create score
-	self.score = Text.create{
-		text = 0,
-		position = scorePosition,
+	self.euroText = Text.create{
+		position = euroPosition,
+		group = groups.hud,
+		referencePoint = CenterRightReferencePoint,
+		size = scoreSize,
+		color = scoreColor
+	}
+
+	self.centsText = Text.create{
+		position = centsPosition,
 		group = groups.hud,
 		referencePoint = CenterRightReferencePoint,
 		size = scoreSize,
@@ -74,6 +86,7 @@ function Class.create(options)
 	}
 
 	-- Start game
+	self:updateScore()
 	self:sendPizza()
 
 	Runtime:addEventListener("addRogueElement", self)
@@ -97,7 +110,8 @@ function Class:destroy()
 		element:destroy()
 	end
 
-	self.score:destroy()
+	self.centsText:destroy()
+	self.euroText:destroy()
 	self.crowd:destroy()
 	self.background:destroy()
 
@@ -113,7 +127,7 @@ end
 -----------------------------------------------------------------------------------------
 
 function Class:sendPizza()
-	local goal = levels[self.currentLevel]
+	self.nbCustomers = levels[self.currentLevel]
 
 	-- Cleaning
 	if self.pizza then
@@ -123,11 +137,28 @@ function Class:sendPizza()
 	-- Create pizza
 	self.pizza = Pizza.create{
 		position = vec2(100, 100),
-		goal = goal
+		goal = self.nbCustomers
 	}
 
-	self.crowd:setCustomers(goal)
+	self.crowd:setCustomers(self.nbCustomers)
 	self.currentLevel = self.currentLevel + 1
+end
+
+function Class:increaseScore(value)
+	self.score = self.score + value
+	self:updateScore()
+end
+
+function Class:updateScore()
+	local euros = math.floor(self.score)
+	local cents = (self.score - euros) * 100
+
+	if cents < 10 then
+		cents = cents.."0"
+	end
+
+	self.euroText:setText(euros..",")
+	self.centsText:setText(cents)
 end
 
 -----------------------------------------------------------------------------------------
@@ -166,6 +197,7 @@ function Class:removeRogueElement(event)
 end
 
 function Class:goalAchieved(event)
+	local goal = 360 / self.nbCustomers
 	local angles = {}
 
 	-- Normalize angles
@@ -184,13 +216,27 @@ function Class:goalAchieved(event)
 		return a < b
 	end)
 
-	-- Add final angle
+	-- Add final angle to complete circle and compute final slice correctly
 	angles[#angles + 1] = angles[1] + 360
 
 	-- Determine tips
 	local previousAngle = angles[1]
 	for i = 2, #angles do
 		local sliceAngle = angles[i] - previousAngle
+		local angularError = abs(sliceAngle - goal)
 		previousAngle = angles[i]
+
+		if angularError <= goal * tipLimits[1] then
+			self:increaseScore(tips[self.nbCustomers][3])
+			print("Perfect!")
+		elseif angularError <= goal * tipLimits[2] then
+			self:increaseScore(tips[self.nbCustomers][2])
+			print("Good!")
+		elseif angularError <= goal * tipLimits[3] then
+			self:increaseScore(tips[self.nbCustomers][1])
+			print("OK")
+		else
+			print("Poor")
+		end
 	end
 end
